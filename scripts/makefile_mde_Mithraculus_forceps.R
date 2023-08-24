e2ade15848488241077d47ea0735d11ffcdd4428
iGregMan <gregoire.maniel@protonmail.com>
2023-08-24 13:01:53 +0200

    modification du boot pour enlever les recherches faites par les ENFA, mise en forme plus propre, création d'un makefile pour détailler les étapes qui conduisent aux données qui seront partagées pour le deuxième livrable (exemple de Mithraculus forceps ici)

--- scripts/makefile_mde_Mithraculus_forceps.R
+++ scripts/makefile_mde_Mithraculus_forceps.R
@@ -2,9 +2,24 @@
 
 # Modèle de distribution d'espèces
 
-# Mithraculus forceps
+# Sélection de l'espèce ----
+# bn <- "Claremontiella nodulosa"
+superfm <- "Majoidea"
+species <- "Mithraculus_forceps"
 
-# Copie des climatologies nécessaires pour le niveau global
-cmd <- "rsync -avuc --delete /home/borea/Documents/mosceco/r_projects/MOSCECO_L2/data_env_prep/data/tidy/clim_global/ /home/borea/Documents/mosceco/r_projects/MOSCECO_L2/habitat_suitability/data/raw/clim_global/"
-system(cmd)
+# Initialisation ----
 source(here::here("scripts", "boot.R"))
+# import des occurrences locales et globales
+# import des climatologies locales
+# copie et import des données environnementales au niveau global (copernicus)
+# modification des données environnementales en une matrice
+# génération des données environnementales issues des ACP
+# séparation dans les climatologies locales des données fixe (habitat) et
+# variables
+# réduction de la colinéarité par vif (déletion de couches de climatologies)
+
+# Modèle de distribution d'espèce ----
+# Première approche (naïve) ----
+# Deuxième approche (ACP) ----
+# Troisième approche (échelles locale et globale, trois niveaux) ----
+# Quatrième approche (hiérarchie des échelles) ----
